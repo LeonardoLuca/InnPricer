59c4ad1e1d5b5ccac1fb94180edba97a0b6af5ec
LeonardoLuca <luca_leonardo@hotmail.com>
2025-05-12 22:09:00 -0300

    Salvar alterações locais antes do cherry-pick

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 
+
 // const api = axios.create({
 //   baseURL: 'http://ecs-innpricer-back-load-balancer-1190307592.us-east-1.elb.amazonaws.com/api/',
 // });
@@ -48,7 +49,7 @@ api.interceptors.response.use(
         // Try to refresh the token
         // const response = await axios.post('http://ecs-innpricer-back-load-balancer-1190307592.us-east-1.elb.amazonaws.com/api/token/refresh/', {
         const response = await axios.post('http://127.0.0.1:8000/api/token/refresh/', {
-
+        
           refresh: refreshToken,
         });
 
